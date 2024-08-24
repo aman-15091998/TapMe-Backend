568ecd8e66f7fbddacb5e616f35bd9c9599875e1
aman-15091998 <amantbitw@gmail.com>
2024-08-24 20:32:16 +0530

    fix

--- server.js
+++ server.js
@@ -3,24 +3,17 @@ import { createServer } from 'http';
 import { schema } from './src/schema.js';
 
 // Create a Yoga instance
-const yoga = createYoga({ schema });
-
-export const server = createServer((req, res) => {
-    // Apply CORS headers
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Adjust origin as needed
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  
-    // Handle preflight requests
-    if (req.method === 'OPTIONS') {
-      res.writeHead(204);
-      res.end();
-      return;
+const yoga = createYoga({
+    schema, 
+    cors: {
+      origin: '*',
+      credentials: true,
+      allowedHeaders: ['*'],
+      methods: ['GET', 'POST']
     }
-  
-    // Delegate request to Yoga
-    yoga.handleRequest(req, res);
-  });
+});
+
+export const server = createServer(yoga);
 
 
 
